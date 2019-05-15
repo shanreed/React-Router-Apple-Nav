fb8b31d27585beda3827610328b1b971709897af
shanreed <srtech5set@gmail.com>
2019-05-15 07:26:12 -0700

    added nav menu items

--- apple-nav/src/component/NavMenu.js
+++ apple-nav/src/component/NavMenu.js
@@ -5,16 +5,16 @@ import './NavMenu.css';
 function NavMenu() {
     return (
       <div className= 'nav-menu'>
-        <NavLink to = '/'><img src = '' alt = ''/></NavLink>
-        <NavLink>Mac</NavLink>
-        <NavLink>iPad</NavLink>
-        <NavLink>iPhone</NavLink>
-        <NavLink>Watch</NavLink>
-        <NavLink>TV</NavLink>
-        <NavLink>Music</NavLink>
-        <NavLink>Support</NavLink>
-        <NavLink><img src = '' alt = ''/></NavLink>
-        <NavLink><img src = '' alt = ''/></NavLink>
+        <NavLink className = 'link apple' to = '/'><i class='fab fa-apple'></i></NavLink>
+        <NavLink className = 'link'>Mac</NavLink>
+        <NavLink className = 'link'>iPad</NavLink>
+        <NavLink className = 'link'>iPhone</NavLink>
+        <NavLink className = 'link'>Watch</NavLink>        
+        <NavLink className = 'link'>TV</NavLink>
+        <NavLink className = 'link'>Music</NavLink>
+        <NavLink className = 'link'>Support</NavLink>
+        <NavLink className = 'link search'><i class="fa fa-search"></i></NavLink>
+        <NavLink className = 'link shop'><i class='fas fa-shopping-bag'></i></NavLink>
       </div>
     );
   }
--- apple-nav/src/component/NavWrapper.js
+++ apple-nav/src/component/NavWrapper.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import NavMenu from './NavMenu';
 import './NavWrapper.css';
 
 function NavWrapper() {
     return (
       <div className= 'nav-wrapper'>
-        <h1>Hello</h1>
+        <NavMenu />
       </div>
     );
   }
